81ed509d57cdc1c4136de7381361adac6da2eaf8
EC2 Default User <ec2-user@ip-172-31-34-2.eu-central-1.compute.internal>
2019-01-24 06:51:11 +0000

    changed code according to MegaFlask tutorial

--- miniform.py
+++ miniform.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, flash, request
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+from wtforms.validators import DataRequired
  
 # App config.
 DEBUG = True
@@ -7,23 +9,24 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
  
-class ReusableForm(Form):
-   name = TextField('Name:', validators=[validators.required()])
+class MyForm(FlaskForm):
+   name = TextField('Name:', validators=[validators.DataRequired()])
  
 @app.route("/", methods=['GET', 'POST'])
 def hello():
-   form = ReusableForm(request.form)
+   form = MyForm()
+   if form.validate_on_submit():
+      flash("test")
+#   print(form.errors)
+#   if request.method == 'POST':
+#      name=request.form['name']
+#      print(name)
  
-   print(form.errors)
-   if request.method == 'POST':
-      name=request.form['name']
-      print(name)
- 
-   if form.validate():
-   # Save the comment here.
-      flash('Hello ' + name)
-   else:
-      flash('All the form fields are required. ')
+#   if form.validate():
+#   # Save the comment here.
+#      flash('Hello ' + name)
+#   else:
+#      flash('All the form fields are required. ')
  
    return render_template('hello.html', form=form)
  
